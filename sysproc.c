139b2318b45eedd51ff38cfd24d53ba8cad7bc5b
sinnna <sharifi.sina1377@gmail.com>
2019-11-05 19:06:21 +0330

    1 step forward

--- sysproc.c
+++ sysproc.c
@@ -88,31 +88,26 @@ sys_sina(void)
 int
 sys_count_num_of_digits(void)
 {
-  long int n;
-  // int num_of_digits = 1;
+  short int n;
+  int num_of_digits = 1;
   char temp;
-
-  // if(argint(0, &n) < 0)
-  //   return -1;
-
-  asm volatile("movl %%ecx, %0" : "=r" (n));
-
-  // while(1)
-  // {
-  //   n = n/10;
-  //   if((n) > 0)
-  //   {
-  //     num_of_digits++;
-  //   }
-  //   else
-  //     break;
-    
-  // }
-  // temp = '0' + num_of_digits;
-  temp = '0' + n;
-
   struct file *f;
-  f=myproc()->ofile[0];
+  f=myproc()->ofile[1];
+
+  asm volatile("movl %%si, %0" : "=r" (n));
+
+  while(1)
+  {
+    n = n/10;
+    if((n) > 0)
+    {
+      num_of_digits++;
+    }
+    else
+      break;
+  }
+  temp = '0' + num_of_digits;
+  
   filewrite(f,  &temp, sizeof(temp));
   return 1;
 }
--- test.c
+++ test.c
@@ -7,14 +7,10 @@
 int
 main(int argc, char *argv[])
 {
-    long int k;
-    k = (long int)atoi(argv[1]);
-    asm volatile("movl %0, %%ecx" : : "r" (k));
-    // asm(
-    //     "movl k, %ax \n"
-    //     // "movl %ax, kk \n"
-    // );
-    // printf(1, "%d \n", k);
+    short int k;
+    k = (int)atoi(argv[1]);
+    asm volatile("movl %0, %%si" : : "r" (k));
+
     count_num_of_digits(atoi(argv[1]));
     exit();
 }
